1dae6beb37950e2a36754a067a5172e7e4ad13c3
Micah <mjayfinley@gmail.com>
2018-12-17 10:06:38 -0600

    heroku test

---
+++ client/build/precache-manifest.7275d3b88c861e10ab4f952c46f308ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eef52cd46e62468f8a91",
+    "url": "/static/js/main.eef52cd4.chunk.js"
+  },
+  {
+    "revision": "e67fb648deb58d4e510e",
+    "url": "/static/js/1.e67fb648.chunk.js"
+  },
+  {
+    "revision": "eef52cd46e62468f8a91",
+    "url": "/static/css/main.6fa4ce91.chunk.css"
+  },
+  {
+    "revision": "360aa6160e9cfeeea46432fd0dc72efb",
+    "url": "/index.html"
+  }
+];
