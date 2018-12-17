305135a35bbb4d9d02713b2c4650534fd4a9b744
Micah <mjayfinley@gmail.com>
2018-12-17 11:55:12 -0600

    sequelize set up

---
+++ models/app.js
@@ -0,0 +1,26 @@
+var express = require('express'), routes = require('./routes'), user = require('./routes/user'), http = require('http'), path = require('path'), db = require('./models');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + './views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+if ('development' === app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('users', user.list);
+
+db.sequelize.sync().then(function() {
+  http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port' + app.get('port'));
+  })
+})
---
+++ models/index.js
@@ -0,0 +1,23 @@
+if (!global.hasOwnProperty('db')) {
+  var Sequelize = require('sequelize'), sequelize = null
+
+  if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      port: match[4],
+      host: match[3],
+      logging: true
+    })
+  } else {
+    sequelize = new Sequelize('example-app-db', 'root', null)
+  }
+
+  global.db = {
+    Sequelize: Sequelize,
+    sequelize: sequelize,
+    User: sequelize.import(__dirname +'/user')
+  }
+}
+
+module.exports = global.db
